7c523bff5937f1c7cee2ce5472c8cb9789d05aa8
Luiz Fernando Vilhena Basso <luiz.basso@tramontina.com>
2022-01-14 16:22:37 -0300

    Add project files.

---
+++ Models/NovoFilmeInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.WebApi.Models
+{
+    public class NovoFilmeInputModel
+    {
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public int Duracao { get; set; }
+        [Required]
+        public string Sinopse { get; set; }
+    }
+}
---
+++ Models/SessaoFilmeInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.WebApi.Models
+{
+    public class SessaoFilmeInputModel
+    {
+        public string NomeFilme { get; set; }
+
+        public int Dia { get; set; }
+
+        public int Mes { get; set; }
+
+        public int Ano { get; set; }
+    }
+}
